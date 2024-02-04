3b906d956d0d5abc396b5580ae2945849d1c5882
Paul Fellows <paul99fellows@gmail.com>
2024-02-04 15:49:11 +0000

    Removed routes from index.js

--- server/index.js
+++ server/index.js
@@ -1,33 +1,10 @@
 const express = require('express');
+const tasks = require("./routes/tasks.js");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static('build'));
-
-const data = [
-    {   
-        id: 1,
-        timestamp: Date.now(),
-        title: 'Get coffee',
-        complete: false,
-    },
-    {   
-        id: 2,
-        timestamp: Date.now(),
-        title: 'Tidy bedroom',
-        complete: false,
-    },
-    {   
-        id: 3,
-        timestamp: Date.now(),
-        title: 'Write code',
-        complete: true,
-    }
-];
-
-app.get('/api/tasks', (req, res) => {
-    res.json(data);
-}); 
+app.use("/api", tasks);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
